cac73f36710359257aebc99f7b55a54a47d0fdca
Szymon Olewniczak <solewniczak@rid.pl>
2019-08-01 16:46:16 +0200

    add "group_by_letters" parameter to plugin syntax that adds letter header before each group

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
-var alphalist2 = {};
-alphalist2.sort_list = function($l) {
-	var a = [];
-	$l.find("> li").each(function() {
-		var $t = jQuery(this);
-		a.push([$t.text(), $t]);
-	});
-	a.sort(function(a, b) { return (a[0]).localeCompare(b[0]) });
-	for (k in a) {
-		var $li = a[k][1];
-		$l.append($li);
-	}
-};
+jQuery(function() {
 
-jQuery(document).ready(function() {
-    jQuery(".alphalist2").find("ol,ul").each(function() {
-        alphalist2.sort_list(jQuery(this));
-    });
+	function sort_list($list) {
+		var elements = [];
+		$list.find("> li").each(function() {
+			var $this = jQuery(this);
+			elements.push([$this.text(), $this]);
+		});
+		elements.sort(function(a, b) { return (a[0]).localeCompare(b[0]) });
+		for (i in elements) {
+			var $li = elements[i][1];
+			$list.append($li);
+		}
+	}
+
+	function group_by_letters($list) {
+		var lists = {};
+
+		$list.find("> li").each(function() {
+			var $this = jQuery(this);
+			var text = $this.text().trim();
+			var firstLetter = text.charAt(0);
+			if (!lists.hasOwnProperty(firstLetter)) {
+				lists[firstLetter] = [];
+			}
+			lists[firstLetter].push($this);
+		});
+
+		var $closestHeaders = $list.closest(":has(:header)").find(":header:first");
+		if ($closestHeaders.length === 0) {
+			var headerLevel = 1;
+		} else {
+			var closestHeader = $closestHeaders[0];
+			var headerLevel = parseInt(closestHeader.tagName.substring(1)) + 1;
+		}
+
+		var $parent = $list.parent();
+		jQuery.each(lists, function (header, elements) {
+			jQuery(document.createElement("h" + headerLevel)).text(header).appendTo($parent);
+			jQuery($list[0].cloneNode()).appendTo($parent).append(elements);
+		});
+		$list.remove();
+	}
+
+	function init() {
+		jQuery(".plugin__alphalist2").each(function() {
+			var $this = jQuery(this);
+			var classes = this.className.split(/\s+/);
+
+			$this.find("ol, ul").each(function () {
+				var $this = jQuery(this);
+				sort_list($this);
+				if (jQuery.inArray("group_by_letters", classes) !== -1) {
+					group_by_letters($this);
+				}
+			});
+		});
+	}
+
+	jQuery(init);
+
 });
--- syntax.php
+++ syntax.php
@@ -11,20 +11,37 @@ class syntax_plugin_alphalist2 extends DokuWiki_Syntax_Plugin {
     public function getType(){ return 'container'; }
     public function getAllowedTypes() { return array('container'); }   
     public function getSort(){ return 158; }
-    public function connectTo($mode) { $this->Lexer->addEntryPattern('<alphalist>(?=.*?</alphalist>)',$mode,'plugin_alphalist2'); }
+    public function connectTo($mode) { $this->Lexer->addEntryPattern('<alphalist.*?>(?=.*?</alphalist>)',$mode,'plugin_alphalist2'); }
     public function postConnect() { $this->Lexer->addExitPattern('</alphalist>','plugin_alphalist2'); }
-    public function handle($match, $state, $pos, Doku_Handler $handler) { return $state; }
+
+    /**
+     * Handle the match
+     */
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
+        switch ($state) {
+            case DOKU_LEXER_ENTER :
+                $attrs = substr($match, strlen('<alphalist'), -1);
+                $classes = preg_split('/\s+/', $attrs, -1, PREG_SPLIT_NO_EMPTY);
+                return array($state, $classes);
+
+            case DOKU_LEXER_UNMATCHED :  return array($state, $match);
+            case DOKU_LEXER_EXIT :       return array($state, '');
+        }
+        return array();    }
 
     /**
      * Create output
      */
-    public function render($mode, Doku_Renderer $renderer, $state) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         // $data is what the function handle() return'ed.
         if($mode == 'xhtml'){
             /** @var Doku_Renderer_xhtml $renderer */
+            list($state, $match) = $data;
             switch ($state) {
-                case DOKU_LEXER_ENTER :      
-                    $renderer->doc .= '<div class="alphalist2">'; 
+                case DOKU_LEXER_ENTER :
+                    $classes = $match;
+                    array_unshift($classes, 'plugin__alphalist2');
+                    $renderer->doc .= '<div class="' . implode(' ', $classes) . '">';
                     break;
                 
                 case DOKU_LEXER_UNMATCHED :  
